05009bd84b960e7c9a83b999a74a6714887575ee
Quentin Bourgeois <quentintgc@live.fr>
2022-03-11 16:32:40 +0100

    shoose

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const choose = document.querySelector('img')
+const shoose = document.querySelector('img')
 const size = document.querySelector('.size')
 
 footSize.addEventListener('input', (e) => {
     size.textContent = e.target.value
-    newChooseSize = choose.width + ((parseInt(e.target.value) * 100) / choose.width)
-    choose.style.width = newChooseSize + 'px'
+    newShooseSize = shoose.width + ((parseInt(e.target.value) * 100) / shoose.width)
+    shoose.style.width = newShooseSize + 'px'
 })
